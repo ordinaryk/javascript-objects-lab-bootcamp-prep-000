4b63fc8070c795748238addcd33a591863371e02
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-31 20:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function  deleteFromObjectByKey(object, key) {
   return object
 }
 
+function deleteFromObjectByKey(object, key) {
+  var newObj = object
+  delete newObj[key]
+  return newObj
+}
+
 function  destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
